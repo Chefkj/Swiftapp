5b678dd7a5f61d68fc9f92660e30c6fcd4feeb59
Chefkj <Kwcbydefeat@gmail.com>
2018-02-23 17:17:06 -0700

    my first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func Button1(_ sender: Any) {
+        theLabel.text = "Hello World!"
+    }
+    @IBAction func Button2(_ sender: Any) {
+        theLabel.text = "Buttons are cool..."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
